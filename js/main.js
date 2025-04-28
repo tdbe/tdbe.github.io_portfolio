4dd870d94654ca91b8467fca86f13afaa1ae3dc2
Tudor Berechet <tudor.berechet@sandbox.game>
2025-04-28 21:49:31 +0300

    added giscus to all project pages lmeow

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function initSetup()
 	});
 	
 
-	if(isMobile() == true || $(window).width() / $(window).height() > 1.77 )
+	if(isMobile() == true)// || $(window).width() / $(window).height() > 1.77 )
 	{
 		return;
 	}
